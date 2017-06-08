c0076ee42d0a4701121c564f6fa68be7fc6f6431
Joseph Parsons <josephtparsons@gmail.com>
2017-06-07 19:45:52 -0500

    372 Assignment 3: Alternate solution

--- 372Assignment3/src/Driver.java
+++ 372Assignment3/src/Driver.java
@@ -1,3 +1,5 @@
+import java.util.EmptyStackException;
+
 /**
  * Created by joseph on 28/05/17.
  */
@@ -28,6 +30,23 @@ public class Driver {
         System.out.println(pushbackTokenizer.nextToken());
 
         System.out.println(pushbackTokenizer.hasMoreTokens() ? "There are still more tokens." : "There are no more tokens.");
+
+        try {
+            pushbackTokenizer.nextToken();
+            System.out.println("No empty stack exception?");
+        } catch (EmptyStackException ex) {
+            System.out.println("Empty stack exception.");
+        }
+
+        pushbackTokenizer.pushback(); // pushes back “test”
+        pushbackTokenizer.pushback(); // pushes back “a”
+        pushbackTokenizer.pushback(); // pushes back “is”
+        pushbackTokenizer.pushback(); // pushes back “this”
+        pushbackTokenizer.pushback(); // pushes back “Hello”
+        pushbackTokenizer.pushback(); // pushes back nothing
+        pushbackTokenizer.pushback(); // pushes back more nothing
+
+        System.out.println(pushbackTokenizer.nextToken()); // "Hello" -- last two pushbacks ignored silently.
     }
 }
 
@@ -44,4 +63,6 @@ test
 a
 test
 There are no more tokens.
+Empty stack exception.
+Hello
  */
--- 372Assignment3/src/PushbackTokenizer.java
+++ 372Assignment3/src/PushbackTokenizer.java
@@ -1,33 +1,50 @@
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.EmptyStackException;
 import java.util.Stack;
 
 /**
- * ...The last assignment was super-duper hard, took me like 8 hours, and this is doable in 20 minutes. Alright.
+ * An implementation of PushbackableTokenizer that uses a second stack for pushback operations.
+ * When a pop occurs from the main stack, a push occurs on the other. When a pushback occurs, we pop from the pushback stack onto the main one.
  */
-
 public class PushbackTokenizer implements PushbackableTokenizer {
-    // The alternative (and probably better) approach is a offset list. But whatever.
+    // There are two ways of doing this while allowing for O(1) pushback and pop operations.
+    // The first approach is an offset list/array implemented using the methods Stack inherits from Vector. This is an okay option, though sort-of seems against the spirit of things.
+    // The second approach is to use two stacks, which does potentially require twice the space -- though I believe Java's Stack and Vector implementations resize freely, so in actuality there is only going to be a little bit of extra space usage for large stacks. If space were enough of a concern, trimToSize() could be invoked after all pops.
+    // My impression, from the assignment, was to use the two stack approach, since the handout indicates to use a stack for _both_ types of parameters: "You must use a Stack object with the actual parameter String to store both types of tokens."
+    // However, I was told by a groupmate who asked if two stacks are okay that that's actually _not_ what we're supposed to do, so I'm leaving the Vector approach as default. The two-stack approach should be pretty clear from the comments, and was developed first. They both work perfectly fine, and, importantly, maintain O(1) operations. I can envision a way of storing pushback values at the back of stack and still using a position to indicate the "bottom" of the accessible stack to implement this using a more "pure stack" approach of only push and pop operations but this is dumb both because we have the Vector get method available, and because doing it that way would be O(n), not O(1), time.
+
     /**
-     * A stack holding the current tokenised values. nextToken() will pop from here, and store in {@link PushbackTokenizer#stackPushBackable}
+     * The "top" of our Stack, when being treated instead as a Vector.
+     * (We only use this for the first method, not the second.)
+     */
+    int pos = 0;
+
+    /**
+     * A stack holding the current tokenised values.
+     * If we're using the two-stack method, nextToken() will pop from here, and store in {@link PushbackTokenizer#stackPushBackable}
      */
     private Stack<String> stack = new Stack<String>();
 
-    /**
-     * A stack holding *used* tokenised values; pushback() will pop from here into {@link PushbackTokenizer#stack}.
-     */
-    private Stack<String> stackPushBackable = new Stack<String>();
+    // /**
+    // * A stack holding *used* tokenised values; pushback() will pop from here into {@link PushbackTokenizer#stack}.
+    // * (We only use this for the second method, not the first.)
+    // */
+    // private Stack<String> stackPushBackable = new Stack<String>();
 
 
     /**
      * Creates a new pushbackable tokenizer by splitting `tokenizable` by `token`.
+     * (The assignment indicated to only have one constructor, but that's silly. If you're tokenising, you're going to have a specific token to tokenise, commonly a space, comma, or slash. Assuming a space -- and not allowing a custom token specification -- is really bad API design.)
      *
      * @param tokenizable The string to be tokenised.
      * @param token The string to use for tokenisation, in any form accepted by {@link String#split(String)}
      */
     public PushbackTokenizer(String tokenizable, String token) {
         stack.addAll(Arrays.asList(tokenizable.split(token)));
-        Collections.reverse(stack);
+
+        // With two stack method only:
+        // Collections.reverse(stack); // Perhaps unsurprisingly, stack.addAll adds them in the wrong order from what we want. So reverse them here.
     }
 
     /**
@@ -45,21 +62,39 @@ public class PushbackTokenizer implements PushbackableTokenizer {
      * @throws java.util.EmptyStackException if there are no more tokens.
      */
     public String nextToken() {
-        return stackPushBackable.push(stack.pop());
+        // With two stacks:
+        // return stackPushBackable.push(stack.pop());
+
+        // With Vector methods:
+        if (pos >= stack.size()) {
+            throw new EmptyStackException();
+        }
+        return stack.get(pos++);
     }
 
     /**
      * {@link PushbackableTokenizer#hasMoreTokens()}
      */
     public boolean hasMoreTokens() {
-        return stack.size() > 0;
+        // With two stacks:
+        // return !stack.empty();
+
+        // With Vector methods:
+        return pos < stack.size();
     }
 
     /**
-     * {@link PushbackableTokenizer#pushback()}
+     * See {@link PushbackableTokenizer#pushback()}
+     * This does allow for pushbacks even when there is no pushback history; it will simply do nothing in such an event.
+     * (I believe this is reasonable behaviour, because no value is expected from this call. Doing the same for a pop() call from an empty stack doesn't have a logically defined behaviour -- at best, it could look at the last value in the pushback stack.)
      */
     public void pushback() {
-        if (stackPushBackable.size() > 0) // Allow pushback() calls past the pushback stack, doing nothing if one occurs.
-            stack.push(stackPushBackable.pop());
+        // With two stacks:
+        //if (stackPushBackable.size() > 0) // Allow pushback() calls past the pushback stack, doing nothing if one occurs.
+        //    stack.push(stackPushBackable.pop());
+
+        // With Vector methods:
+        pos--;
+        if (pos < 0) pos = 0; // Allow pushback() calls past the pushback stack, doing nothing if one occurs.
     }
 }
