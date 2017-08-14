60c7deb420a2a4e313fbf0af9bac79af85983916
Joseph Parsons <josephtparsons@gmail.com>
2017-08-14 12:32:31 -0500

    Prog D: A few small fixes, writing output to file.

--- 340-progD/src/ProgD.java
+++ 340-progD/src/ProgD.java
@@ -4,7 +4,8 @@ import java.util.*;
 
 /**
  * Simulation of local search over a set of rules.
- * Note that I never tested with anything other than COURSES_PER_SEMESTER = 3, nor with any different number of courses. While I think all the arithmetic is right, I could have an off-by-one error here-or-there that will manifest with different courses or courses per semester. (Indeed, while it's possible a solution doesn't exist with COURSES_PER_SEMESTER > 3, I haven't been able to find one for =1 or =2 either. So there's probably a bug somewhere.)
+ * Note that I never fully tested with anything other than COURSES_PER_SEMESTER = 3, nor with any different number of courses. While I think all the arithmetic is right, I could have an off-by-one error here-or-there that will manifest with different courses or courses per semester.
+ * (Also, last minute edits were done outside of my usual development environment. Apologies for occasionally incosistent indentation.)
  *
  * @author Joseph T. Parsons
  */
@@ -15,7 +16,13 @@ public class ProgD {
      * Getting 4 and 5-per-semester working requires adding a few more "empty" courses in order to find a satisfying assignment.
      * (I haven't been able to get 1 working with the SemesterRestrictionsRule. I think it's just too random in that case, since 499 must be in a very specific semester.)
      */
-    static final int COURSES_PER_SEMESTER = 1;
+    static final int COURSES_PER_SEMESTER = 3;
+    
+    /**
+     * The maximum number of solutions that will be tested in verbose mode.
+     * (I don't want to be responsible for any uneccessary harddrive fragmentation if something goes wrong, thank you very much.)
+     */
+    static final int MAXIMUM_ATTEMPTS = 10000;
 
     /**
      * Whether to print verbose or summary.
@@ -23,7 +30,7 @@ public class ProgD {
     private static boolean printVerbose = false;
 
     /**
-     * Passes to ProgBGraphical, which launches file opener.
+     * Prompts for output mode and then runs very specific local search simulation.
      */
     public static void main(String[] args) {
         // Ask for print mode.
@@ -36,20 +43,22 @@ public class ProgD {
                 break;
             }
             else if (printModePrompt.toLowerCase().startsWith("v")) {
-                System.out.println("Using verbose mode.");
+                System.out.println("Using verbose mode. No more than 10,000 attempts to find a solution will be made; if you are running with default parameters, a solution will almost always be found in less than 2,000 attempts. If you are not, we do not want to fill your drive.");
                 printVerbose = true;
                 break;
             }
 
             System.out.println("I did not understand that. Please enter 'v' for verbose mode, or 's' for short mode.");
             printModePrompt = UserInterfacePrompts.promptLine("Would you like to print in verbose or short mode? ");
-        }
+        };
 
 
         // Redirect Output to File
         // (Personally, I believe this should be done by the user through use of piping, but I'm just following instructions.)
         try {
-            System.setOut(new PrintStream(new File("ProgD-solution.txt")));
+            File outFile = new File("ProgD-solution.txt");
+            System.out.println("The output will be written to ProgD-solution.txt in your current working directory. Its full path is " + outFile.getAbsolutePath());
+            System.setOut(new PrintStream(outFile));
         } catch (Exception e) {
             System.err.println("Unable to open file for writing. Output will be to stdout.");
         }
@@ -61,7 +70,7 @@ public class ProgD {
         Department nullDepartment = new Department(""); // Used for dummy courses.
 
 
-        // Create list of courses. (A hashmap is used to more easily specify each course by its code.)
+        // Create list of courses. (A hashmap is used to more easily reference each course by its code. Kinda like an associative array in other, typically weaker-typed languages. A tree map is used to get the list of courses as sorted.)
         Map<Integer, Course> courses = new TreeMap<>();
         courses.put(120, new Course(mathDepartment, 120));
         courses.put(210, new Course(mathDepartment, 210));
@@ -123,16 +132,15 @@ public class ProgD {
 
         // Create our special rules.
         prereqs.addRule(new FreshmanSophomoreBeforeSeniorRule());
-        //System.out.println((int) Math.ceil((double) courseListSorted.size() / COURSES_PER_SEMESTER) - 1);
         prereqs.addRule(new SemesterRestrictionsRule(COURSES_PER_SEMESTER, (int) Math.ceil((double) courseListSorted.size() / COURSES_PER_SEMESTER) - 1)); // Create a semester rule. Second parameter calculates the number of semesters needed to take the current list of courses, assuming COURSES_PER_SEMESTER is the maximum number taken per semester.
 
 
-        // Randomise the list of courses.
+        // Randomise the list of courses. (Assignment specified use of Math.random, but Collections.shuffle seems more appropriate here. Also, it's a lot easier.)
         List<Course> courseList = new ArrayList<>(courses.values());
         Collections.shuffle(courseList);
 
 
-        // Print Course Titles as header if in verbose mode
+        // Print Course titles as header if in verbose mode
         if (printVerbose) {
             for (int i = 0; i < courseListSorted.size(); i++) {
                 System.out.print(courseListSorted.get(i) + "\t");
@@ -146,15 +154,21 @@ public class ProgD {
 
 
         // Find Conflicts, Swap Stuff, and Repeat
-        List<Map.Entry<Integer, Integer>> conflicts;
+        int attempts = 0; // Number of times a solution is tested.
+        List<Map.Entry<Integer, Integer>> conflicts; // List of conflicts found on every attempt.
         while ((conflicts = prereqs.findConflicts(courseList, COURSES_PER_SEMESTER)).size() != 0) { // Basically, keep going until no conflicts recorded.
+            if (printVerbose && attempts++ > MAXIMUM_ATTEMPTS) { // Stop if too many attempts and we're in verbose mode.
+                System.out.println("Maximum attempts reached. In an effort to spare your harddrive, we're going to stop now. No solution has been found.");
+                break;
+            }
+            
             if (printVerbose) {
                 coursePrint(courseListSorted, courseList); // Print the assignment of courses that was tested.
             }
 
             int randomIndex = (int) (Math.random() * conflicts.size()); // Choose one conflict at random.
 
-            Collections.swap(courseList, conflicts.get(randomIndex).getKey(), conflicts.get(randomIndex).getValue()); // Flip the two courses involved in the random conflict.
+            Collections.swap(courseList, conflicts.get(randomIndex).getKey(), conflicts.get(randomIndex).getValue()); // Flip the two courses involved in the random conflict. (Under some circumstances, this will be one course involved in a conflict and another chosen at random.)
         }
 
 
@@ -169,7 +183,7 @@ public class ProgD {
 
 
     /**
-     * Print the numerical semester assigned to a given course (starting at one),
+     * Print the semester number assigned to a given course (starting at one, as per assignment example).
      * @param courseListSorted The list of courses in the order that they should be printed.
      * @param courseList The list of courses in semester-assigned order.
      */
@@ -196,7 +210,7 @@ public class ProgD {
         // Sort each semester individually for printing
         // (As an aside, there's been a lot more arithmetic on this assignment than I'd expected.)
         for (int i = 0; i < coursesList.size() / COURSES_PER_SEMESTER; i++) {
-            Collections.sort(coursesList.subList(i * COURSES_PER_SEMESTER, (((i + 1) * COURSES_PER_SEMESTER) > coursesList.size() ? coursesList.size() : ((i + 1) * COURSES_PER_SEMESTER))));
+            Collections.sort(coursesList.subList(i * COURSES_PER_SEMESTER, (((i + 1) * COURSES_PER_SEMESTER) > coursesList.size() ? coursesList.size() : ((i + 1) * COURSES_PER_SEMESTER)))); // For instance, this will sort the sublist of [0, 2] elements, then [3, 5], [6, 8], and so-on.
         }
 
 
